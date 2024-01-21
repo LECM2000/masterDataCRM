b0b9a2571049856e8b2a1a8f76bf08bc1870c1d8
Luis Enrique Corona <luisenrique.corona@kuantik.mx>
2024-01-21 09:52:05 -0600

    fix router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,9 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./features/dashboard/dashboard.module').then(m => m.DashboardModule) }, 
   { path: 'dashboard', loadChildren: () => import('../../features/dashboard/dashboard.module').then(m => m.DashboardModule) },
  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
- { path: 'perfil', loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilModule) }];
- { path: 'share', loadChildren: () => import('./share/share.module').then(m => m.ShareModule) }, { path: 'perfil', loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilModule) }];
+ { path: 'perfil', loadChildren: () => import('./perfil/perfil.module').then(m => m.PerfilModule) }, 
+ { path: 'share', loadChildren: () => import('./share/share.module').then(m => m.ShareModule) }];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
