321d071b354837928fef33421f0c6ddd2e129742
Luis Enrique Corona <luisenrique.corona@kuantik.mx>
2024-01-28 11:08:25 -0600

    feat: cambios para subir a producción

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
@@ -17,7 +16,6 @@ import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production:true,
+    firebase: {
+      apiKey: "AIzaSyCqsbCoqsWmc6TG8MUkXshG2moPp3vgZuA",
+      authDomain: "conekta-2c642.firebaseapp.com",
+      projectId: "conekta-2c642",
+      storageBucket: "conekta-2c642.appspot.com",
+      messagingSenderId: "263765737105",
+      appId: "1:263765737105:web:e8cf1af2cacd0eea770930",
+      measurementId: "G-3DM4H09M11"
+    },
+  };
